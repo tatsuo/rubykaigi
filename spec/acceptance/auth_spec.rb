310146115c672b8a5cc62232c94a9b2bf059495c
hibariya <celluloid.key@gmail.com>
2011-07-02 19:57:24 +0900

    add specs

--- spec/acceptance/auth_spec.rb
+++ spec/acceptance/auth_spec.rb
@@ -1,11 +1,5 @@
 require 'acceptance/acceptance_helper'
 
-shared_context 'signout after all' do
-  after :all do
-    click_link 'Sign out' rescue nil # XXX scenarioが上から順番に実行されるわけではないらしいので必要。rescue nilどうにかしたい
-  end
-end
-
 feature 'ユーザとして初めてOAuthでログインする' do
   include_context 'signout after all'
 
