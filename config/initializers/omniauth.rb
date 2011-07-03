1d64f710059768d51725171cb220ab6cb2217cc7
hibariya <celluloid.key@gmail.com>
2011-07-03 17:26:22 +0900

    whitespaces

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -22,14 +22,14 @@ module OmniAuth
   module Strategies
     class Password
       include OmniAuth::Strategy
-      
+
       def initialize(app, secret = 'changethisappsecret', options = {}, &block)
         @secret = secret
         super(app, :password, options, &block)
       end
 
       attr_reader :secret
-      
+
       def request_phase
         return fail!(:missing_information) unless request[:identifier] && request[:password]
         return fail!(:password_mismatch) if request[:password_confirmation] && request[:password_confirmation] != '' && request[:password] != request[:password_confirmation]
@@ -38,7 +38,7 @@ module OmniAuth
         env['omniauth.auth'] = auth_hash(encrypt(request[:identifier], request[:password]))
         call_app!
       end
-      
+
       def auth_hash(crypted_password)
         OmniAuth::Utils.deep_merge(super(), {
           'uid' => crypted_password,
@@ -47,11 +47,11 @@ module OmniAuth
           }
         })
       end
-      
+
       def callback_phase
         call_app!
       end
-      
+
       def encrypt(identifier, password)
         Digest::SHA1.hexdigest([identifier, password, secret].join('::'))
       end
