b7a56d1355a95499657b2ccb36a21a0552bd6b2a
Ryunosuke SATO <tricknotes@yahoo.co.jp>
2011-07-11 09:20:19 +0900

    fix schedule

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rubykaigi::Application.routes.draw do
     resources :advent_calendar, :only => %w(index)
     match 'sponsors_ruby/:id', :to => 'sponsors_ruby#show'
     match 'schedule/grid' => 'schedule#grid'
+    match 'schedule/details/:id' => 'schedule#details', :as => 'schedule_details'
     match 'schedule/phone_index' => 'schedule#phone_index'
     match 'schedule/phone_details/:id' => 'schedule#phone_details', :as => 'schedule_phone_details'
     match 'schedule/phone_list/:id' => 'schedule#phone_list', :as => 'schedule_phone_list'
